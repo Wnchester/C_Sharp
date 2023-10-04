20b9f6d334c5c3c25bb145caf4132b14d26345d5
Servet Tütüncü <127959025+Wnchester@users.noreply.github.com>
2023-10-04 21:06:26 +0300

    C#Snake

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace yılanoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new System.Windows.Forms.Panel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.Color.White;
+            panel1.Location = new System.Drawing.Point(137, 78);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(800, 510);
+            panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(985, 78);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(83, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Skor:";
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(985, 165);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(122, 96);
+            button1.TabIndex = 1;
+            button1.Text = "Tekrar Oyna";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Vivaldi", 19.8000011F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(476, 18);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(113, 41);
+            label2.TabIndex = 2;
+            label2.Text = "Snake";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.Cyan;
+            ClientSize = new System.Drawing.Size(1192, 600);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
